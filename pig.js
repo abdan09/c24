f21a87bfb79ca62e8e0f70bf3927074c829875db
abdan09 <77271420+abdan09@users.noreply.github.com>
2021-02-13 18:57:35 +0530

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,28 @@
+class Pig {
+    constructor(x, y) {
+      var options = {
+          'restitution':0.8,
+          'friction':1,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, 60,60, options);
+      this.width = 60;
+      this.height = 60;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(5)
+      stroke("brown");
+      fill("pink");
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
+  
